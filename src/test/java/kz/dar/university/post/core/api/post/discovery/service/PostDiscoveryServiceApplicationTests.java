c13b128ea50ff59969d8613013f40338e85eeb08
Alibek <nurmahanovalibek@gmail.com>
2023-08-07 23:55:31 +0600

    Initial commit

---
+++ src/test/java/kz/dar/university/post/core/api/post/discovery/service/PostDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.university.post.core.api.post.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
